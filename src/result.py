8fc7901df5b49333053ffc04d78765cd3d495900
lyubolp <lyubo.karev@gmail.com>
2022-12-03 23:23:58 +0200

    Added template for the result class

---
+++ src/result.py
@@ -0,0 +1,15 @@
+"""
+The result.py module contains the Result class, used for representing search results
+"""
+
+class Result:
+    """
+    Class representing the result from a search.
+    Contains the line number of the match, the column from which the match starts, the line itself, and N amount of lines before and after the match.
+    """
+
+    def __init__(self) -> None:
+        pass
+
+    def __str__(self) -> str:
+        pass
